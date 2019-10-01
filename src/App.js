f6f3db70f151e54697f0cb8bc065acfbacbf9b8c
Nell <kuney93@naver.com>
2019-10-01 14:59:55 -0400

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
 import Customer from './components/Customer'
+import './App.css';
+import Paper from '@material-ui/core/Table'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customer = [{
   'id': 1,
@@ -28,24 +46,40 @@ const customer = [{
 }]
 
 class App extends Component {
+  
   render() {
-  return (
+    const { classes } = this.props;
+    return (
     <div>
-    {
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+      <TableHead>
+        <TableRow>
+          <TableCell>Number</TableCell>
+          <TableCell>Image</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>BirthDay</TableCell>
+          <TableCell>Gender</TableCell>
+          <TableCell>Job</TableCell>
+        </TableRow>
+      </TableHead>{
       customer.map(c => {
         return (
           <Customer
-          key={c.id}
+            key={c.id}
             id={c.id}
             image={c.image}
             name={c.name}
             birthday={c.birthday}
+            gender={c.gender}
             job={c.job} />
         )
       })
     }
+    </Table>
+    </Paper>
     </div>
     );
   }
 }
-export default App;
+export default withStyles(styles)(App);
